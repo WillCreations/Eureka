8c2b4d30488dc2096ab52e76b5d4f41d86c79baf
Princewill <princewilligwe15@gmail.com>
2025-10-03 21:38:30 +0100

    further emailing adjustments

--- src/app/(Engine)/actions/emailAction.tsx
+++ src/app/(Engine)/actions/emailAction.tsx
@@ -1,5 +1,5 @@
-import { sendEmail } from "@/app/api/recovery/route";
-
+import nodemailer from "nodemailer";
+import { google } from "googleapis";
 export const emailAction = async (formData) => {
   "use server";
 
@@ -12,17 +12,119 @@ export const emailAction = async (formData) => {
       subject,
       message,
     } = Object.fromEntries(formData);
-    const stats = "client";
-    return sendEmail({
+    console.log({
       firstName,
       lastName,
       recepient_Email,
+      oldMessage,
       subject,
       message,
-      oldMessage,
-      stats,
     });
+
+    if (!firstName || !lastName || !recepient_Email || !subject || !message) {
+      throw new Error("All fields are required");
+    }
+    const REPLY_URI = process.env.REPLY_URI;
+    const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+    const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+    const REFRESH_TOKEN = process.env.GOOGLE_REFRESH_TOKEN;
+    const USER_EMAIL = process.env.GMAIL_ADDRESS;
+    const REDIRECT_URI = process.env.REDIRECT_URI;
+
+    console.log({ REFRESH_TOKEN });
+    console.log({ CLIENT_SECRET });
+    console.log({ CLIENT_ID });
+    console.log({ USER_EMAIL });
+    console.log({ REDIRECT_URI });
+    const oAuth2Client = new google.auth.OAuth2(
+      CLIENT_ID,
+      CLIENT_SECRET,
+      REDIRECT_URI
+    );
+    oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+    const accessToken = await oAuth2Client.getAccessToken();
+    console.log({ accessToken });
+    console.log("before transporter has fired");
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: USER_EMAIL,
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken: accessToken,
+      },
+    });
+
+    const result = await transporter.sendMail({
+      from: `Eureka: <${USER_EMAIL}>`,
+      to: recepient_Email,
+      subject: subject,
+      html: `
+          <div style="font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px;">
+            <div style="max-width: 600px; margin: 0 auto; background: #ffffff; border-radius: 8px; overflow: hidden; box-shadow: 0 2px 5px rgba(0,0,0,0.1);">
+              
+              <div style="background-color: #4CAF50; color: white; text-align: center; padding: 16px; font-size: 20px; font-weight: 500px">
+                Eureka
+              </div>
+      
+              <div style="padding: 20px; color: #333;">
+                <p style="font-size: 16px; line-height: 1.5; margin-bottom: 20px;">
+                  Hi <strong>${firstName + " " + lastName}</strong>,
+                </p>
+                <p style="font-size: 14px; line-height: 1.6; margin-bottom: 20px; ">
+                  You sent us a message.
+                </p>
+                <p style="font-size: 14px; line-height: 1.6; margin-bottom: 20px;">
+                 Thank you for reaching out to us. Here is a copy of your message:
+                  <br />
+                  <blockquote>
+                    <strong>"${oldMessage}"</strong>
+                    <cite>– ${firstName + " " + lastName}</cite>
+                  </blockquote>
+                  <br />
+                  <br />
+                </p>
+                <p style="font-size: 14px; line-height: 1.6; margin-bottom: 20px;">
+                   ${message}
+                </p>
+                <p style="font-size: 12px; line-height: 1.0; margin-bottom: 5px;">
+                   Click the link below to sign up and get started!
+                </p>
+                <a href="${REPLY_URI}/register"  
+                target="_blank" rel="noopener noreferrer">
+                  <p 
+                   style="display: inline-block; background-color: #4CAF50; color: #fff; text-decoration: none; padding: 12px 20px; border-radius: 4px; font-size: 16px;">
+                   Sign Up
+                  </p>
+                </a>
+                <a href="${REPLY_URI}"  
+                target="_blank" rel="noopener noreferrer">
+                  <p 
+                   style="display: inline-block; background-color: #4CAF50; color: #fff; text-decoration: none; padding: 12px 20px; border-radius: 4px; font-size: 16px;">
+                   See Our Services
+                  </p>
+                </a>
+                
+                <p style="font-size: 12px; color: #888; margin-top: 20px;">
+                  If you are no longer interested or are already a client, you can ignore this email.
+                </p>
+              </div>
+            </div>
+          </div>
+        `,
+    });
+    console.log("Email sent:", result);
+
+    return {
+      ok: true,
+      message: result,
+    };
   } catch (error) {
     console.log(error);
+    return { ok: false, message: error.message };
   }
 };
--- src/app/api/recovery/route.jsx
+++ src/app/api/recovery/route.jsx
@@ -1,10 +1,147 @@
 import { NextResponse } from "next/server";
 import { connectToDb } from "@/app/(Engine)/mongodb/database";
 import User from "@/app/(Engine)/models/user";
-import { sendEmail } from "@/app/(Engine)/actions/sendEmail";
 
-// import nodemailer from "nodemailer";
-// import { google } from "googleapis";
+import nodemailer from "nodemailer";
+import { google } from "googleapis";
+
+const sendEmail = async (body) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      OTP,
+      recepient_Email,
+      subject,
+      stats,
+      oldMessage,
+      message,
+    } = body;
+    const REPLY_URI = process.env.REPLY_URI;
+    const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+    const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+    const REFRESH_TOKEN = process.env.GOOGLE_REFRESH_TOKEN;
+    const USER_EMAIL = process.env.GMAIL_ADDRESS;
+    const REDIRECT_URI = process.env.REDIRECT_URI;
+
+    console.log({ body });
+    console.log({ REFRESH_TOKEN });
+    console.log({ CLIENT_SECRET });
+    console.log({ CLIENT_ID });
+    console.log({ USER_EMAIL });
+    console.log({ REDIRECT_URI });
+    const oAuth2Client = new google.auth.OAuth2(
+      CLIENT_ID,
+      CLIENT_SECRET,
+      REDIRECT_URI
+    );
+    oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+    const accessToken = await oAuth2Client.getAccessToken();
+    console.log({ accessToken });
+    console.log("before transporter has fired");
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: USER_EMAIL,
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken: accessToken,
+      },
+    });
+
+    console.log("after transporter has fired");
+    let tempUser;
+
+    if (stats === "login" || stats === "resend") {
+      const user = await User.findOne({ email: recepient_Email });
+      if (user) {
+        tempUser = user;
+      }
+    }
+
+    let html;
+
+    switch (stats) {
+      case "register":
+        html = `
+    <div style="font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px;">
+      <div style="max-width: 600px; margin: 0 auto; background: #ffffff; border-radius: 8px; overflow: hidden; box-shadow: 0 2px 5px rgba(0,0,0,0.1);">
+        
+        <div style="background-color: #4CAF50; color: white; text-align: center; padding: 16px; font-size: 20px;">
+          Welcome to Our Service
+        </div>
+
+        <div style="padding: 20px; color: #333;">
+          <p style="font-size: 16px; line-height: 1.5; margin-bottom: 20px;">
+            Hi <strong>There</strong>,
+          </p>
+          <p style="font-size: 14px; line-height: 1.6; margin-bottom: 20px;">
+             Please confirm your email address by Entering the OTP below:
+          </p>
+          <p 
+             style="display: inline-block; background-color: #4CAF50; color: #fff; text-decoration: none; padding: 12px 20px; border-radius: 4px; font-size: 16px;">
+             ${OTP}
+          </p>
+          <p style="font-size: 12px; color: #888; margin-top: 20px;">
+            If you didn’t request this, you can ignore this email.
+          </p>
+        </div>
+      </div>
+    </div>
+  `;
+        break;
+      case "login":
+        html = `
+    <div style="font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px;">
+      <div style="max-width: 600px; margin: 0 auto; background: #ffffff; border-radius: 8px; overflow: hidden; box-shadow: 0 2px 5px rgba(0,0,0,0.1);">
+        
+        <div style="background-color: #4CAF50; color: white; text-align: center; padding: 16px; font-size: 20px;">
+          Welcome to Our Service
+        </div>
+
+        <div style="padding: 20px; color: #333;">
+          <p style="font-size: 16px; line-height: 1.5; margin-bottom: 20px;">
+            Hi <strong>${
+              tempUser !== undefined ? tempUser.name : "There"
+            }</strong>,
+          </p>
+          <p style="font-size: 14px; line-height: 1.6; margin-bottom: 20px;">
+             Please confirm your email address by Entering the OTP below:
+          </p>
+          <p 
+             style="display: inline-block; background-color: #4CAF50; color: #fff; text-decoration: none; padding: 12px 20px; border-radius: 4px; font-size: 16px;">
+             ${OTP}
+          </p>
+          <p style="font-size: 12px; color: #888; margin-top: 20px;">
+            If you didn’t request this, you can ignore this email.
+          </p>
+        </div>
+      </div>
+    </div>
+  `;
+        break;
+      default:
+        html = `<div>No Mail</div>`;
+    }
+
+    const result = await transporter.sendMail({
+      from: `Eureka: <${USER_EMAIL}>`,
+      to: recepient_Email,
+      subject: subject,
+      html,
+    });
+    console.log("Email sent:", result);
+
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw new Error("email Failed");
+  }
+};
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
@@ -24,44 +161,3 @@ export async function POST(request) {
     return new NextResponse(JSON.stringify(error), { status: 500 });
   }
 }
-
-// const email_Config = };
-
-// const sendEmail = async (body) => {
-//   const { recepient_Email, OTP } = body;
-//   try {
-//     const accessToken = oAuth2Client.getAccessToken();
-//     return new Promise((resolve, reject) => {
-//       const transporter = nodemailer.createTransport({
-//         service: "gmail",
-//         auth: {
-//           type: "OAuth2",
-//           user: USER_EMAIL,
-//           clientId: CLIENT_ID,
-//           clientSecret: CLIENT_SECRET,
-//           refreshToken: REFRESH_TOKEN,
-//           accessToken: accessToken.token,
-//         },
-//       });
-
-//       transporter.sendMail(
-//         {
-//           from: "princewilligwe15@gmail.com",
-//           to: recepient_Email,
-//           subject: "Recover Password",
-//           text: `Enter your OTP code: ${OTP} `,
-//         },
-//         (error) => {
-//           if (error) {
-//             console.log(error);
-//             return reject({ message: "An error occurred" });
-//           }
-//           return resolve({ message: "Email sent successfully" });
-//         }
-//       );
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     throw new Error("email Failed");
-//   }
-// };
